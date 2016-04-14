0f76755b071308daa0496f78bac7d56986c37ffc
Ben Zörb <ben@sommerlaune.com>
2016-04-14 17:26:33 +0200

    Update to loadcss v1.2.0 + cssrelpreload

--- index.js
+++ index.js
@@ -10,9 +10,9 @@
  */
 'use strict';
 
-var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
+var _ = require('lodash');
 var UglifyJS = require('uglify-js');
 var cave = require('cave');
 var reaver = require('reaver');
@@ -23,7 +23,11 @@ var CleanCSS = require('clean-css');
 var slash = require('slash');
 var normalizeNewline = require('normalize-newline');
 var resolve = require('resolve');
-var loadCSS = read(resolve.sync('fg-loadcss'));
+
+var loadCssMain = resolve.sync('fg-loadcss');
+var loadCssBase = path.dirname(loadCssMain);
+
+var loadCSS = read(loadCssMain) + read(path.join(loadCssBase, 'cssrelpreload.js'));
 loadCSS = UglifyJS.minify(loadCSS, {fromString: true}).code;
 
 /**
@@ -44,6 +48,16 @@ function read(file) {
     return fs.readFileSync(file, 'utf8');
 }
 
+function filter($, options, reject) {
+    return function (index, element) {
+        var href = $(element).attr('href');
+        var i = _.findIndex(options.ignore, function (arg) {
+            return _.isRegExp(arg) && arg.test(href) || arg === href;
+        });
+        return reject ? i === -1 : i !== -1;
+    };
+}
+
 module.exports = function (html, styles, options) {
     var $ = cheerio.load(String(html), {
         decodeEntities: false
@@ -54,20 +68,17 @@ module.exports = function (html, styles, options) {
     });
 
     var o = options || {};
-    var target = o.selector || links.get(0) || $('script').get(0);
+    var target = o.selector || $('link[rel="preload"]').get(0) || links.get(0) || $('script').get(0);
     var $target = $(target);
 
     if (_.isString(o.ignore)) {
         o.ignore = [o.ignore];
     }
 
+    var ignored = $();
     if (o.ignore) {
-        links = links.filter(function () {
-            var href = $(this).attr('href');
-            return _.findIndex(o.ignore, function (arg) {
-                return _.isRegExp(arg) && arg.test(href) || arg === href;
-            }) === -1;
-        });
+        ignored = links.filter(filter($, o));
+        links = links.filter(filter($, o, true));
     }
 
     // minify if minify option is set
@@ -104,23 +115,21 @@ module.exports = function (html, styles, options) {
             });
         }
 
+        // add ignored elements right above the processed ones
+        noscript.before(ignored);
+
         // wrap links to stylesheets in noscript block so that they will evaluated when js is turned off
         links.each(function (idx) {
             var el = $(this);
             el.attr('href', hrefs[idx]);
             noscript.append(el);
             noscript.append('\n');
+            noscript.before('<link rel="preload" href="' + hrefs[idx] + '" as="style" onload="this.rel=\'stylesheet\'">\n');
         });
 
         // build js block to load blocking stylesheets and insert it right before
         // exposes async stylesheets as global asyncss array which could be used with onloadCSS
-        noscript.before('<script id="loadcss">\n' +
-            loadCSS + '\n' +
-            '(function(w,u,s){' +
-            'w.asyncss=[];' +
-            'for(var i in u){w.asyncss.push(w.loadCSS(u[i],s));}' +
-            '}("undefined"!=typeof global?global:this,[\'' + hrefs.join('\',\'') + '\'],document.getElementById("loadcss")));\n' +
-            '</script>\n');
+        noscript.after('\n<script id="loadcss">\n' + loadCSS + '\n</script>\n');
     }
 
     var dom = parse($.html());
